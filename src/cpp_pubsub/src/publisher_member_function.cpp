0fd1c3d7e0174708b5a2ca94a8859a8b99a9eafb
Valerie Bonte <bonte.valeri@gmail.com>
2023-07-01 16:32:28 -0400

    Test

--- src/cpp_pubsub/src/publisher_member_function.cpp
+++ src/cpp_pubsub/src/publisher_member_function.cpp
@@ -37,6 +37,7 @@ public:
 private:
   void timer_callback()
   {
+    
     auto message = std_msgs::msg::String();
     message.data = "Hello, world! " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
